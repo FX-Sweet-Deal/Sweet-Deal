3cf1aa53b329d973af24dbfc33320c7e509976e2
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-05-19 15:05:59 +0900

    chore: rename goods to item
    chore: rename goods to item
    
    - goods -> item 으로 패키지명 변경 및 모듈 settings

--- item/src/main/java/com/example/item/controller/ItemController.java
+++ item/src/main/java/com/example/item/controller/ItemController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GoodsController {
+public class ItemController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Goods Service!";
+        return "Hello from Item Service!";
     }
 }
