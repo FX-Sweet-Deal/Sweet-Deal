46514d20049286006a37c915258eb9b382c5d969
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-05-21 11:07:42 +0900

    feat: modify Item Status

--- item/src/main/java/com/example/item/domain/ItemStatus.java
+++ item/src/main/java/com/example/item/domain/ItemStatus.java
@@ -1,7 +1,8 @@
 package com.example.item.domain;
 
 public enum ItemStatus {
-    SALE,
-    RESERVED,
-    UNRESERVED;
+    SALE, // 판매 중
+    SOLD, // 판매 됨
+    RESERVED, // 예약 됨
+    DELETED; // 삭제 됨
 }
