6c3b57726814aadd35c13a9c6cd29e66ad627c09
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-05-21 14:56:08 +0900

    feat: 상품 조회 로직 추가

--- item/src/main/java/com/example/item/repository/ItemRepository.java
+++ item/src/main/java/com/example/item/repository/ItemRepository.java
@@ -1,23 +1,18 @@
-//package com.example.item.repository;
-//
-//import com.example.item.domain.Item;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//public interface ItemRepository extends JpaRepository<Item, Long> {
-//
-//    Item add(Long id, Item storeId);
-//
-//    /** 단일 조회 **/
-//    Optional<Item> findById(Long id);
-//    Optional<Item> findByName(String name);
-//
-//    List<Item> findAll();
-//
-//
-//
-//
-//
-//}
+package com.example.item.repository;
+
+import com.example.item.domain.Item;
+import com.example.item.domain.ItemStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    Optional<Item> findByIdAndStatus(Long itemId, ItemStatus status);
+
+    List<Item> findByStoreIdAndStatusOrderByIdDesc(Long storeId, ItemStatus status);
+
+    List<Item> findAllByStatus(ItemStatus status);
+
+}
