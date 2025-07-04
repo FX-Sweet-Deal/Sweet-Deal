8788ed0ee8c2fe080ba1f99cf105d6c86995b982
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-04 20:10:37 +0900

    feat: add item Error code

--- global/src/main/java/com/example/global/errorcode/ItemErrorCode.java
+++ global/src/main/java/com/example/global/errorcode/ItemErrorCode.java
@@ -1,5 +1,6 @@
 package com.example.global.errorcode;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,12 @@ public enum ItemErrorCode implements ErrorCode {
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 1100, "상품을 찾을 수 없습니다."),
     INVALID_ITEM_INFO(HttpStatus.BAD_REQUEST.value(), 1101, "상품 정보가 유효하지 않습니다."),
     ITEM_UPDATE_DENIED(HttpStatus.FORBIDDEN.value(), 1102, "상품 수정 권한이 없습니다."),
-    FAIL_CREATE_ITEM(HttpStatus.INTERNAL_SERVER_ERROR.value(),1103, "상품 등록에 실패했습니다.");
+    FAIL_CREATE_ITEM(HttpStatus.INTERNAL_SERVER_ERROR.value(),1103, "상품 등록에 실패했습니다."),
+    ITEM_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), 1104, "상품이 이미 존재합니다."),
+    ITEM_DELETE_DENIED_SOLD(HttpStatus.FORBIDDEN.value(), 1105, "판매된 상품은 삭제할 수 없습니다."),
+    ITEM_DELETE_DENIED_ALREADY(HttpStatus.BAD_REQUEST.value(), 1106, "이미 삭제된 상품입니다."),
+    ITEM_ALREADY_REGISTERED(HttpStatus.CONFLICT.value(), 1107, "이미 등록된 상품입니다.");
+
 
     private final Integer httpCode;
     private final Integer errorCode;
