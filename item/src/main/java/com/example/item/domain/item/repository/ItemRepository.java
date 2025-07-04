5706a8f6aad2103f509ff726e9ac9c4e5d1f91d0
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-04 20:13:38 +0900

    feat: add method for automatic cleanup

--- item/src/main/java/com/example/item/domain/item/repository/ItemRepository.java
+++ item/src/main/java/com/example/item/domain/item/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.example.item.domain.item.repository;
 
 import com.example.item.domain.item.repository.enums.ItemStatus;
+import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -19,16 +20,20 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     중복 검증
     같은 스토어에 동일한 이름 유통기한 상품이 존재하는지 검증
      */
-    Boolean existsByStoreIdAndNameAndExpiredAtAndStatus(Long storeId, String name, LocalDateTime expiredAt, ItemStatus status);
+    Boolean existsByStoreIdAndNameAndExpiredAtAndStatus(Long storeId, String name,
+        LocalDateTime expiredAt, ItemStatus status);
 
-    /*
-    상태별 & 유통기한 체크
-    유통기한이 expiredAt 이후인 상품을 조회할 때
-     */
-    Optional<Item> findByIdAndStatusAndExpiredAtAfter(Long itemId, ItemStatus status, LocalDateTime expiredAtAfter);
+    // SALE or RESERVED 중 유통기한 만료된 상품 조회
+    List<Item> findAllByStatusInAndExpiredAtBefore(List<ItemStatus> status,
+        LocalDateTime expiredAtBefore);
+
+    List<Item> findAllByStatusAndUnregisteredAtBefore(ItemStatus status,
+        LocalDateTime unregisteredAtBefore);
 
     // 특정 id와 상태로 조회
-    Optional<Item> findAllByIdAndStatus(Long itemId, ItemStatus status);
+    Optional<Item> findByIdAndStatus(Long itemId, ItemStatus status);
 
+    List<Item> findByStoreIdAndStatusOrderByIdDesc(Long storeId, ItemStatus status);
 
 }
+
