26bc75b70e66c069b4a77cc4c25ee53a41db5405
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-04 20:22:37 +0900

    feat: add ItemUpdateRequest DTO for updating item details

---
+++ item/src/main/java/com/example/item/domain/item/controller/model/update/ItemUpdateRequest.java
@@ -0,0 +1,34 @@
+package com.example.item.domain.item.controller.model.update;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+public class ItemUpdateRequest {
+
+  @NotNull
+  @Pattern(
+      regexp = "^[가-힣A-Za-z0-9]{1,100}$",
+      message = "한글, 영문, 숫자 및 공백만 입력 가능합니다."
+  )
+  private String name;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  @DateTimeFormat(iso = ISO.DATE_TIME)
+  @FutureOrPresent(message = "지정된 일시는 현재 또는 미래여야 합니다.")
+  private LocalDateTime expiredAt;
+
+  @Pattern(regexp = "^[0-9]$", message = "숫자만 입력할 수 있습니다.")
+  private Long price;
+
+  @Pattern(regexp = "^[0-9]$", message = "숫자만 입력할 수 있습니다.")
+  private Integer quantity;
+
+
+
+
+}
