845c7502eb1ed8940534175a0b907d07750e7560
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-10 23:38:20 +0900

    feat: 공통 에러 코드 수정 및 추가

--- global/src/main/java/com/example/global/errorcode/CommonErrorCode.java
+++ global/src/main/java/com/example/global/errorcode/CommonErrorCode.java
@@ -7,20 +7,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
-
-    /*
-    임시 작성 중
+     /*
+    수정 필요
      */
 
-
-    //== 200 ==//
-    SUCCESS(HttpStatus.OK.value(), 200, "OK"),
-
-    //== 400 ==//
-    NOT_SUPPORTED_HTTP_METHOD(HttpStatus.BAD_REQUEST.value(),1, "Http Method Failed"),
-    NOT_VALID_METHOD_ARGUMENT(HttpStatus.BAD_REQUEST.value(),1, "REQUEST를 확인해주세요"),
-    BAD_PASSWORD(HttpStatus.BAD_REQUEST.value(), 1, "비밀번호가 일치하지 않습니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), 1, "해당 사용자를 찾을 수 없습니다.");
+    OK(HttpStatus.OK.value(), 200, "성공"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "서버 내부에 에러가 발생했습니다."),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), 512, "NULL POINT 입니다."),
+    INVALID_INPUT_DATA(HttpStatus.BAD_REQUEST.value(), 400, "유효하지 않은 입력입니다."),
+    MISSING_REQUIRED_HEADER(HttpStatus.BAD_REQUEST.value(), 431, "필수 헤더값이 누락 또는 만료되었습니다."),
+    MISSING_REQUIRED_PARAM(HttpStatus.BAD_REQUEST.value(), 431, "필수 파라미터값이 누락되었습니다."),
+    ASYNC_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "비동기 처리 중 에러가 발생했습니다."),
+    DB_PROCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), 500, "DB 처리 중 에러가 발생했습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), 401, "접근 권한이 없습니다.");
 
     private final Integer httpCode;
     private final Integer errorCode;
