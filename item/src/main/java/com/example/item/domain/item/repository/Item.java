5338816be07bd76eb68967b91014411478af0828
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-10 23:42:16 +0900

    feat: Item id 전략 변경(IDENTITY)

--- item/src/main/java/com/example/item/domain/item/repository/Item.java
+++ item/src/main/java/com/example/item/domain/item/repository/Item.java
@@ -9,13 +9,13 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "item")
 public class Item {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, name = "item_id")
     private Long id;
 
@@ -70,6 +70,47 @@ public class Item {
 
     public void quantityIncrease(int quantity) {
         getQuantity().increase(quantity);
+    }
+
+    public void updateQuantity(int quantity) {
+        if(quantity <= 0) {
+            throw new IllegalArgumentException("상품 갯수는 1개 이상이어야 합니다."); // 예외 수정 할것
+        }
+        this.quantity = new PositiveIntegerCount(quantity);
+    }
+
+    public void rename(String name) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 비어 있을 수 없습니다."); // 예외 수정 할것
+        }
+
+        this.name = name;
+    }
+
+    public void updateExpiredAt(LocalDateTime expiredAt) {
+        LocalDateTime present = LocalDateTime.now();
+
+        if(expiredAt.isBefore(present)) {
+            throw new IllegalArgumentException("유통기한은 현재 날짜 이후여야 합니다."); // 예외 수정 할것
+        }
+
+        this.expiredAt = expiredAt;
+    }
+
+    public void updatePrice(Long price) {
+        if(price < 0) {
+            throw new IllegalArgumentException("가격은 0원 이상이어야 합니다."); // 예외 수정 할것
+        }
+
+        this.price = price;
+    }
+
+    public void updateStatus(ItemStatus status) {
+        if(status == ItemStatus.DELETED || status == ItemStatus.SOLD) {
+            throw new IllegalArgumentException("삭제되거나 판매된 상품은 상태를 변경할 수 없습니다.");
+        }
+
+        this.status = status;
     }
 
 }
