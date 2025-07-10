86afc36d9ef07add3416ca918a484ef18b370527
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-10 23:46:03 +0900

    feat: ItemExceptionHandler 예외 발생 처리

---
+++ item/src/main/java/com/example/item/domain/common/exception/ItemExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.example.item.domain.common.exception;
+
+import com.example.global.api.Api;
+import com.example.global.errorcode.ItemErrorCode;
+import com.example.item.domain.common.exception.item.ItemAlreadyExistsException;
+import com.example.item.domain.common.exception.item.ItemCannotDeleteException;
+import com.example.item.domain.common.exception.item.ItemNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ItemExceptionHandler {
+
+  @ExceptionHandler(value = ItemNotFoundException.class)
+  public ResponseEntity<Api<Object>> itemNotFoundException(ItemNotFoundException e) {
+    log.info("", e);
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(Api.error(ItemErrorCode.ITEM_NOT_FOUND));
+  }
+
+  @ExceptionHandler(value = ItemAlreadyExistsException.class)
+  public ResponseEntity<Api<Object>> itemAlreadyExistsException(ItemAlreadyExistsException e) {
+    log.info("", e);
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(Api.error(ItemErrorCode.ITEM_ALREADY_EXISTS));
+  }
+
+  @ExceptionHandler(value = ItemCannotDeleteException.class)
+  public ResponseEntity<Api<Object>> itemCannotDeleteException(ItemCannotDeleteException e) {
+    log.info("", e);
+    return ResponseEntity.status(HttpStatus.CONFLICT)
+        .body(Api.error(ItemErrorCode.ITEM_ALREADY_DELETED));
+  }
+
+
+
+}
