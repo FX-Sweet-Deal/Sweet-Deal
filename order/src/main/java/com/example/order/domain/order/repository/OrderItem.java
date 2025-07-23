bb351b26f7e79bc810acfacb5035747701ecc95a
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-23 11:23:44 +0900

    feat: OrderItem 클래스 생성

---
+++ order/src/main/java/com/example/order/domain/order/repository/OrderItem.java
@@ -0,0 +1,38 @@
+package com.example.order.domain.order.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "order_item_id")
+  private Long id;
+
+  @Column(nullable = false)
+  private Long quantity;
+
+  @Column(nullable = false)
+  private Long orderPrice;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id", nullable = false)
+  private Orders orderId;
+
+  @Column(name = "item_id")
+  private Long itemId;
+}
