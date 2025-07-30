902564c275c76b7f5710bf5d1b27f97b8d24665d
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-30 22:55:07 +0900

    feat: 주문 취소 요청 DTO 생성

---
+++ order/src/main/java/com/example/order/domain/order/controller/model/request/CancelRequest.java
@@ -0,0 +1,15 @@
+package com.example.order.domain.order.controller.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@RequiredArgsConstructor
+public class CancelRequest {
+  private Long id;
+
+}
