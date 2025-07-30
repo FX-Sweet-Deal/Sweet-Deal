234196aaa83afd4cb7f576221c3571a49e9795c2
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-30 23:12:49 +0900

    feat: 주문 등록 요청 DTO 생성

---
+++ order/src/main/java/com/example/order/domain/order/controller/model/request/OrderItemRequest.java
@@ -0,0 +1,18 @@
+package com.example.order.domain.order.controller.model.request;
+
+import com.example.order.domain.order.repository.Orders;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OrderItemRequest {
+  private Long quantity;
+  private Long itemId;
+
+
+}
