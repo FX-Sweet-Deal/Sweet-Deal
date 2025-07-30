4fe31b0a5a1d7e0986eb33253597023a54ba0312
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-07-30 23:15:47 +0900

    feat: 결제 방식 구현

---
+++ order/src/main/java/com/example/order/domain/order/repository/enums/PaymentMethod.java
@@ -0,0 +1,15 @@
+package com.example.order.domain.order.repository.enums;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum PaymentMethod {
+
+  CREDIT_CARD("신용 카드"),
+  DEBIT_CARD("직불 카드"),
+  PAYPAL("페이팔"),
+  BANK_TRANSFER("계좌 이체");
+
+  final String description;
+
+}
