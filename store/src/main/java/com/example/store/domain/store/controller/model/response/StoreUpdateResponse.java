d759767b9583b86c42734a1f68df87f7767a7ea2
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-06 17:57:12 +0900

    feat: 스토어 정보 업데이트 응답 DTO 구현

---
+++ store/src/main/java/com/example/store/domain/store/controller/model/response/StoreUpdateResponse.java
@@ -0,0 +1,14 @@
+package com.example.store.domain.store.controller.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreUpdateResponse {
+
+}
