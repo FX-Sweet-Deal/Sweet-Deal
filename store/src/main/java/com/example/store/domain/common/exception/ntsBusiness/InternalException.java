61bc1d5e4914383409e3e9f6a12c1a3508cf13c3
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-12 13:32:57 +0900

    feat: Internal Server Error 예외 구현

--- store/src/main/java/com/example/store/domain/common/exception/ntsBusiness/InternalException.java
+++ store/src/main/java/com/example/store/domain/common/exception/ntsBusiness/InternalException.java
@@ -2,7 +2,7 @@ package com.example.store.domain.common.exception.ntsBusiness;
 
 import com.example.global.errorcode.ErrorCode;
 
-public class InternalException extends OdcloudApiException {
+public class InternalException extends OpenApiException {
     public InternalException(ErrorCode errorCode) {
         super(errorCode);
     }
