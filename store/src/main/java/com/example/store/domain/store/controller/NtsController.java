66d260242bcf7bbfc4eb2b0072786ed6fc2a2de7
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-12 13:31:03 +0900

    feat: 사업자등록정보 진위확인 controller 구현

--- store/src/main/java/com/example/store/domain/store/controller/NtsController.java
+++ store/src/main/java/com/example/store/domain/store/controller/NtsController.java
@@ -3,8 +3,12 @@ package com.example.store.domain.store.controller;
 import com.example.global.api.Api;
 import com.example.store.domain.store.controller.model.request.BusinessStatusRequest;
 import com.example.store.domain.store.controller.model.request.BusinessValidateRequest;
+import com.example.store.domain.store.controller.model.response.BusinessStatusResponse;
+import com.example.store.domain.store.controller.model.response.BusinessValidateResponse;
 import com.example.store.domain.store.service.OpenApiClient;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,23 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class NtsController {
 
-  private OpenApiClient openApiClient;
-
+  private final OpenApiClient openApiClient;
 
   // 진위 확인
-  @PostMapping("/validate")
-  public Api<String> validate(@RequestBody BusinessValidateRequest req) {
-    String json = openApiClient.validate(req.getB_no(), req.getStart_dt(), req.getP_nm());
-    return Api.ok(json);
+  @PostMapping("/validate") // 200
+  public ResponseEntity<BusinessValidateResponse> validate(@Valid @RequestBody BusinessValidateRequest req) {
+    BusinessValidateResponse res = openApiClient.validate(req);
+    return ResponseEntity.ok(res);
   }
 
   // 상태 조회
-  @PostMapping("/status")
-  public Api<String> status(@RequestBody BusinessStatusRequest req) {
-    String json = openApiClient.status(req.getBusinessNos());
-    return Api.ok(json);
+  @PostMapping("/status") // 200
+  public ResponseEntity<BusinessStatusResponse> status(@RequestBody BusinessStatusRequest req) {
+    BusinessStatusResponse res = openApiClient.status(req);
+    return ResponseEntity.ok(res);
   }
-
-
 
 }
