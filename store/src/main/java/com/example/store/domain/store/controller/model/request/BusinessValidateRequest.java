e4b567b8848dbef9587f0b8975e932dbec6f34cb
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-12 13:26:54 +0900

    feat: 사업자등록정보 진위확인 요청 DTO 구현

--- store/src/main/java/com/example/store/domain/store/controller/model/request/BusinessValidateRequest.java
+++ store/src/main/java/com/example/store/domain/store/controller/model/request/BusinessValidateRequest.java
@@ -1,23 +1,18 @@
 package com.example.store.domain.store.controller.model.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import com.example.store.domain.store.repository.Business;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class BusinessValidateRequest {
 
-  @Pattern(regexp="\\d{10}", message="사업자번호는 숫자 10자리")
-  private String b_no; // 사업자등록번호 (필수)
-
-  @Pattern(regexp="\\d{8}", message="개업일자는 YYYYMMDD 8자리")
-  private String start_dt; // 개업일자 (필수)
-
-  @NotBlank(message = "대표자명")
-  private String p_nm; // 대표자성명1: 홍길동 (필수)
+  private List<Business> businesses;
 
 }
