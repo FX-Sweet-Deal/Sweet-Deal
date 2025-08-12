b4537e751d15229cd4e4fd1274d0fc458a915f26
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-12 13:26:10 +0900

    feat: 사업자등록정보 상태조회 요청 DTO 구현

--- store/src/main/java/com/example/store/domain/store/controller/model/request/BusinessStatusRequest.java
+++ store/src/main/java/com/example/store/domain/store/controller/model/request/BusinessStatusRequest.java
@@ -1,13 +1,18 @@
 package com.example.store.domain.store.controller.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class BusinessStatusRequest {
-  private List<String> businessNos;
+
+  @JsonProperty("b_no")
+  private List<String> bNo;
 }
