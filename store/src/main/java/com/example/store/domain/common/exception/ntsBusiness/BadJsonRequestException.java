523708aca7ff15162c421e4fd095ebc8f153b3b1
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-12 13:24:46 +0900

    feat: JSON 포맷에 적합하지 않은 요청 예외 구현

---
+++ store/src/main/java/com/example/store/domain/common/exception/ntsBusiness/BadJsonRequestException.java
@@ -0,0 +1,15 @@
+package com.example.store.domain.common.exception.ntsBusiness;
+
+import com.example.global.errorcode.ErrorCode;
+
+public class BadJsonRequestException extends OdcloudApiException{
+
+    public BadJsonRequestException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public BadJsonRequestException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+
+}
