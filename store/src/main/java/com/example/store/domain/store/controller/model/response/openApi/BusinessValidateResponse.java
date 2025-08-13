5644e9057e78dbe3d5b62be6c23d698ae419bdc9
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-13 17:11:15 +0900

    feat: Business Validate Response DTO 구현

---
+++ store/src/main/java/com/example/store/domain/store/controller/model/response/openApi/BusinessValidateResponse.java
@@ -0,0 +1,21 @@
+package com.example.store.domain.store.controller.model.response.openApi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+@JsonPropertyOrder({ "status_code", "request_cnt", "valid_cnt", "data" }) // ← 순서 지정
+public class BusinessValidateResponse {
+
+  @JsonProperty("status_code")
+  private String statusCode;
+  @JsonProperty("request_cnt")
+  private int requestCnt;
+  @JsonProperty("valid_cnt")
+  private int validCnt;
+
+  private List<InfoResponse> data;
+
+}
