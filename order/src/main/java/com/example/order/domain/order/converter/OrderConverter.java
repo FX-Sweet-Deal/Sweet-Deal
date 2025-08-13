d7e675d72c86f34d2922727af157748843c939a0
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-14 01:00:10 +0900

    feat: OrderResponse DTO 변환 메서드 구현

--- order/src/main/java/com/example/order/domain/order/converter/OrderConverter.java
+++ order/src/main/java/com/example/order/domain/order/converter/OrderConverter.java
@@ -2,9 +2,13 @@ package com.example.order.domain.order.converter;
 
 import com.example.global.anntation.Converter;
 import com.example.order.domain.order.controller.model.request.OrderItemRequest;
+import com.example.order.domain.order.controller.model.response.OrderItemResponse;
 import com.example.order.domain.order.controller.model.response.OrderRegisterResponse;
+import com.example.order.domain.order.controller.model.response.OrderResponse;
 import com.example.order.domain.order.repository.OrderItem;
 import com.example.order.domain.order.repository.Orders;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Converter
 public class OrderConverter {
@@ -21,7 +25,7 @@ public class OrderConverter {
         .paymentId(order.getPayment().getPaymentId())
         .paymentStatus(order.getPayment().getPaymentStatus())
         .paymentMethod(order.getPaymentMethod())
-        .paymentAt(order.getPayment().getPaymentAt())
+        .paymentAt(null)
         .totalPayPrice(order.calculateTotalPrice())
         .paymentSuccess(order.isPaymentSuccess())
         .build();
@@ -32,6 +36,31 @@ public class OrderConverter {
         .itemId(orderItem.getItemId())
         .quantity(orderItem.getQuantity())
         .build();
+  }
+
+  public List<OrderResponse> toOrderResponse(List<Orders> ordersList) {
+    return ordersList.stream().map(orders ->
+        OrderResponse.builder()
+            .id(orders.getId())
+            .orderedAt(orders.getOrderedAt())
+            .cancelledAt(orders.getCancelledAt())
+            .status(orders.getStatus())
+            .totalPrice(orders.getTotalPrice())
+            .userId(orders.getUserId())
+            .storeId(orders.getStoreId())
+            .build())
+        .collect(Collectors.toList());
+  }
+
+  public List<OrderItemResponse> toOrderItemResponse(List<OrderItem> orderItems) {
+    return orderItems.stream().map(orderItem ->
+        OrderItemResponse.builder()
+            .id(orderItem.getId())
+            .quantity(orderItem.getQuantity())
+            .price(orderItem.getOrderId())
+            .totalPrice(orderItem.getTotalPrice())
+            .itemId(orderItem.getItemId()).build()).toList();
+
   }
 
 }
