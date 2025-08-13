a7b00577e7a8c3570d429421aad350d79d64569e
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-14 00:59:25 +0900

    feat: 스토어에서 주문 취소 기능 구현

--- order/src/main/java/com/example/order/domain/order/business/OrderBusiness.java
+++ order/src/main/java/com/example/order/domain/order/business/OrderBusiness.java
@@ -2,21 +2,26 @@ package com.example.order.domain.order.business;
 
 import com.example.global.anntation.Business;
 import com.example.global.api.Api;
+import com.example.global.errorcode.OrderErrorCode;
+import com.example.order.domain.common.exception.order.ExpiredException;
+import com.example.order.domain.common.exception.order.ItemCountNotEnoughException;
+import com.example.order.domain.common.exception.order.ItemNotExistFoundException;
+import com.example.order.domain.common.exception.order.InvalidOrderException;
+import com.example.order.domain.common.exception.order.StoreNotExistException;
 import com.example.order.domain.common.response.MessageConverter;
 import com.example.order.domain.common.response.MessageResponse;
-import com.example.order.domain.order.controller.model.request.CancelRequest;
 import com.example.order.domain.order.controller.model.request.MessageUpdateRequest;
 import com.example.order.domain.order.controller.model.request.OrderItemRequest;
 import com.example.order.domain.order.controller.model.request.PaymentRequest;
+import com.example.order.domain.order.controller.model.response.OrderItemResponse;
+import com.example.order.domain.order.controller.model.response.OrderResponse;
+import com.example.order.domain.order.controller.model.response.StoreSimpleResponse;
 import com.example.order.domain.order.converter.OrderConverter;
 import com.example.order.domain.order.controller.model.response.OrderRegisterResponse;
-import com.example.order.domain.order.controller.model.request.OrderItemRegisterRequest;
+import com.example.order.domain.order.controller.model.request.OrderRegisterRequest;
 import com.example.order.domain.order.repository.OrderItem;
-import com.example.order.domain.order.repository.OrderItemRepository;
 import com.example.order.domain.order.repository.Orders;
-import com.example.order.domain.order.repository.Payment;
 import com.example.order.domain.order.repository.enums.OrderStatus;
-import com.example.order.domain.order.repository.enums.PaymentMethod;
 import com.example.order.domain.order.service.ItemFeignClient;
 import com.example.order.domain.order.service.OrderItemService;
 import com.example.order.domain.order.service.OrderService;
@@ -24,14 +29,17 @@ import com.example.order.domain.order.service.StoreFeignClient;
 import com.example.order.domain.order.service.model.item.ItemInform;
 import com.example.order.domain.order.service.model.item.ItemInternalRequest;
 import com.example.order.domain.order.service.model.item.ItemInternalResponse;
+import feign.FeignException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Business
+@Slf4j
 @RequiredArgsConstructor
 public class OrderBusiness {
 
@@ -41,79 +49,97 @@ public class OrderBusiness {
   private final StoreFeignClient storeFeignClient;
   private final OrderConverter orderConverter;
   private final MessageConverter messageConverter;
-  private final OrderItemRepository orderItemRepository;
 
-  public OrderRegisterResponse order(OrderItemRegisterRequest orderItemRegisterRequests,
-      PaymentMethod paymentMethod, Long userId) { // 임시 userId
+  public OrderRegisterResponse order(OrderRegisterRequest orderRegisterRequest,
+       Long userId) { // 임시 userId
     Orders order = Orders.builder()
-        .payment(Payment.builder().paymentMethod(paymentMethod).build())
-        .userId(userId)
+        .storeId(orderRegisterRequest.getStoreId())
+        .userId(userId) // 임시
         .totalPrice(0L)
         .build();
     orderService.order(order);
 
-    List<OrderItemRequest> orderItemRequests = orderItemRegisterRequests.
+    List<OrderItemRequest> orderItemRequests = orderRegisterRequest.
         getOrderItems().stream().toList();
 
-    boolean isValid = createOrderItems(orderItemRequests, order);// order 객체에 orderItem 저장됨.
+    boolean isValid = createOrderItems(orderItemRequests, order); // order 객체에 orderItem 저장됨.
     if (!isValid) {
-      throw new IllegalArgumentException("주문이 취소되었습니다.");
+      throw new InvalidOrderException(OrderErrorCode.INVALID_ORDER_REQUEST);
     }
 
-    return orderConverter.toRegisterResponse(order);
+    orderService.initPaymentMethod(order, orderRegisterRequest.getPaymentMethod());
+    OrderRegisterResponse registerResponse = orderConverter.toRegisterResponse(order);
 
+    return registerResponse;
   }
 
   private boolean createOrderItems(List<OrderItemRequest> orderItemRequests, Orders order) {
     List<Long> itemIds = orderItemRequests.stream()
         .map(OrderItemRequest::getItemId)
-        .toList();
+        .toList(); // 상품의 id 리스트
 
     ItemInternalRequest itemInternalRequest = new ItemInternalRequest(itemIds);
-    Api<ItemInternalResponse> response = itemFeignClient.getItem(itemInternalRequest);
+    Api<ItemInternalResponse> response = itemFeignClient.getItem(itemInternalRequest); // 상품의 정보를 불러옴.
     ItemInternalResponse itemInternalResponse = response.result();
 
     if (itemInternalResponse == null) {
-      return false;
+      throw new ItemNotExistFoundException(OrderErrorCode.ITEM_NOT_EXIST);
     }
 
-    List<ItemInform> itemInforms = itemInternalResponse.getItemInforms();
-    for (ItemInform itemInform : itemInforms) {
+    for (OrderItemRequest orderItemRequest : orderItemRequests) {
+      ItemInform info = itemInternalResponse.getItemInforms().stream()
+          .filter(itemInform -> itemInform.getId().equals(orderItemRequest.getItemId()))
+          .findFirst()
+          .orElseThrow(() -> new InvalidOrderException(OrderErrorCode.INVALID_ORDER_REQUEST));
 
-      for (OrderItemRequest orderItemRequest : orderItemRequests) {
-        if (itemInform.getQuantity() < orderItemRequest.getQuantity()) {
-          return false;
-        }
-        if (itemInform.getExpiredAt().isBefore(LocalDateTime.now())) {
-          return false;
-        }
+      if (info.getQuantity() < orderItemRequest.getQuantity()) {
+        long available = info.getQuantity();
+        long requested = orderItemRequest.getQuantity();
 
-        OrderItem orderItem = OrderItem.builder()
-            .quantity(itemInform.getQuantity())
-            .price(itemInform.getPrice())
-            .totalPrice(itemInform.getPrice() * itemInform.getQuantity())
-            .orderId(order)
-            .itemId(itemInform.getId())
-            .build();
+        throw new ItemCountNotEnoughException(
+            OrderErrorCode.ITEM_COUNT_NOT_ENOUGH,
+            String.format("상품 재고가 부족합니다. 남은 재고: %d개, 요청 수량: %d개", available, requested));
+      }
 
-        orderItemRepository.save(orderItem);
-
+      if (info.getExpiredAt().isBefore(LocalDateTime.now())) {
+        throw new ExpiredException(OrderErrorCode.ITEM_EXPIRED);
       }
     }
 
+    for (OrderItemRequest orderItemRequest : orderItemRequests) {
+      ItemInform info = itemInternalResponse.getItemInforms().stream()
+          .filter(itemInform -> itemInform.getId().equals(orderItemRequest.getItemId()))
+          .findFirst()
+          .get();
+
+      OrderItem orderItem = OrderItem.builder()
+          .quantity(info.getQuantity())
+          .price(info.getPrice())
+          .totalPrice(info.getPrice() * info.getQuantity())
+          .itemId(info.getId())
+          .build();
+
+      log.info("{}", orderItem.toString()); // 통신 성공!
+
+      orderService.addOrderItem(order, orderItem);
+      orderService.calculateTotalPrice(order);
+      orderItemService.save(orderItem);
+
+    }
     return true;
   }
 
-
-  public MessageResponse completePayment(PaymentRequest paymentRequest, Long userId) { // 임시 userId
-    Orders order = orderService.getOrderByOrderIdAndStatus(paymentRequest.getOrderId(),
+  public MessageResponse completePayment(Long orderId, PaymentRequest paymentRequest, Long userId) { // 임시 userId
+    Orders order = orderService.getOrderByOrderIdAndStatus(orderId,
         OrderStatus.PENDING_PAYMENT); // 결제 대기중인 주문 조회
 
     // 결제 성공(가정)
-    orderService.completePayment(order);
+    orderService.completePayment(order, paymentRequest.isSuccess());
 
     // 유저 유효성 검사
-
+    if (order.getId() != userId) {
+      throw new IllegalArgumentException("USER_NOT_EQUAL");
+    }
 
     List<OrderItem> orderItems = orderItemService.findByOrderId(order.getId());
     List<Long> itemIds = orderItems.stream().map(OrderItem::getItemId).toList();
@@ -122,7 +148,7 @@ public class OrderBusiness {
     ItemInternalResponse itemInternalResponse = response.result();
 
     if (itemInternalResponse == null) {
-      throw new IllegalArgumentException("ITEM NOT FOUND");
+      throw new ItemNotExistFoundException(OrderErrorCode.ITEM_NOT_EXIST);
     }
 
     Map<Long, ItemInform> itemInformMap = itemInternalResponse.getItemInforms().stream()
@@ -130,15 +156,19 @@ public class OrderBusiness {
             Function.identity()));
 
     for (OrderItem orderItem : orderItems) {
-      ItemInform itemInform = itemInformMap.get(orderItem.getId());
+      ItemInform itemInform = itemInformMap.get(orderItem.getItemId());
 
       if(itemInform.getQuantity() < orderItem.getQuantity()) {
-        throw new IllegalArgumentException("");
+        long available = itemInform.getQuantity();
+        long requested = orderItem.getQuantity();
 
+        throw new ItemCountNotEnoughException(
+            OrderErrorCode.ITEM_COUNT_NOT_ENOUGH,
+            String.format("상품 재고가 부족합니다. 남은 재고: %d개, 요청 수량: %d개", available, requested));
       }
 
       if(itemInform.getExpiredAt().isBefore(LocalDateTime.now())) {
-        throw new IllegalArgumentException("유통기한이 지났습니다.");
+        throw new ExpiredException(OrderErrorCode.ITEM_EXPIRED);
       }
     }
 
@@ -153,11 +183,14 @@ public class OrderBusiness {
     return messageConverter.toResponse("결제가 완료되었습니다.");
   }
 
-  public MessageResponse cancelOrder(CancelRequest cancelRequest, Long userId) {
-    Orders order = orderService.getOrderByOrderId(cancelRequest.getId());
+  // 유저가 주문을 취소
+  public MessageResponse cancelOrder(Long orderId, Long userId) {
+    Orders order = orderService.getOrderByOrderId(orderId);
 
     // 유저 유효성 검사
-
+    if(order.getId() != userId) {
+      throw new IllegalArgumentException("USER_NOT_EQUAL");
+    }
 
     List<OrderItem> orderItems = orderItemService.findByOrderId(order.getId());
 
@@ -170,8 +203,70 @@ public class OrderBusiness {
     return messageConverter.toResponse("주문이 취소되었습니다.");
   }
 
-  public MessageResponse cancelStoreOrder(CancelRequest cancelRequest, Long userId) {
-    return null;
+  // 스토어에서 주문을 취소
+  public MessageResponse cancelStoreOrder(Long orderId, Long userId) {
+
+    try {
+      Api<StoreSimpleResponse> storeSimpleResponseApi = storeFeignClient.getStore(userId);
+      StoreSimpleResponse response = storeSimpleResponseApi.result();
+      Orders order = orderService.getOrderByOrderId(orderId);
+
+      // 유저 유효성 검사
+      if (order.getId() != userId) {
+        throw new IllegalArgumentException("USER_NOT_EQUAL");
+      }
+      orderService.cancel(order);
+      return messageConverter.toResponse("주문이 취소되었습니다.");
+
+    }
+    catch (FeignException e) {
+      throw new StoreNotExistException(OrderErrorCode.STORE_NOT_EXIST);
+    }
+
+  }
+
+  // 유저의 주문 리스트
+  public List<OrderResponse> getOrder(Long userId){
+    List<Orders> ordersList = orderService.getOrderListByUserId(userId);
+    return getOrderResponses(ordersList);
+  }
+
+
+  // 스토어 점주가 조회하는 주문 리스트
+  public List<OrderResponse> getStoreOrder(Long userId) {
+    try {
+      Api<StoreSimpleResponse> storeSimpleResponseApi = storeFeignClient.getStore(userId);
+      StoreSimpleResponse response = storeSimpleResponseApi.result();
+      List<Orders> orders = orderService.getOrderListByStoreId(response.getStoreId());
+      return getOrderResponses(orders);
+    }
+    catch (FeignException e) {
+      throw new StoreNotExistException(OrderErrorCode.STORE_NOT_EXIST);
+    }
+
+  }
+
+  // 유저의 주문 리스트
+  private List<OrderResponse> getOrderResponses(List<Orders> ordersList) {
+
+    return ordersList.stream().map(orders ->
+    {
+      List<OrderItem> orderItems = orderItemService.findByOrderId(orders.getId());
+      List<OrderItemResponse> orderItemResponse = orderConverter.toOrderItemResponse(orderItems);
+
+      return OrderResponse.builder()
+          .id(orders.getId())
+          .orderedAt(orders.getOrderedAt())
+          .cancelledAt(orders.getCancelledAt())
+          .status(orders.getStatus())
+          .totalPrice(orders.getTotalPrice())
+          .userId(orders.getUserId())
+          .userId(orders.getUserId())
+          .orderItemResponses(orderItemResponse)
+          .build();
+    })
+        .toList();
+
   }
 
 }
