de6d99e4d5c7b70e7a6c6511017062d9f8fe6df7
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-14 15:35:37 +0900

    feat: 스토어 에러코드 추가

--- global/src/main/java/com/example/global/errorcode/StoreErrorCode.java
+++ global/src/main/java/com/example/global/errorcode/StoreErrorCode.java
@@ -12,7 +12,8 @@ public enum StoreErrorCode implements ErrorCode {
      */
 
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 1050, "스토어를 찾을 수 없습니다."),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN.value(), 1051, "스토어 접근 권한이 없습니다.");
+    ACCESS_DENIED(HttpStatus.FORBIDDEN.value(), 1051, "스토어 접근 권한이 없습니다."),
+    IS_BLANK(HttpStatus.BAD_REQUEST.value(), 1052, "공백은 허용하지 않습니다.");
 
     private final Integer httpCode;
     private final Integer errorCode;
