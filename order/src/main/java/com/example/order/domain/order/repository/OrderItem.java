208c980e4ed7f9b142c146449cc7cb0edfaa4005
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-14 14:17:30 +0900

    feat: 직렬화 시 orderId로 직렬화 메서드 추가

--- order/src/main/java/com/example/order/domain/order/repository/OrderItem.java
+++ order/src/main/java/com/example/order/domain/order/repository/OrderItem.java
@@ -1,5 +1,7 @@
 package com.example.order.domain.order.repository;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -35,9 +37,30 @@ public class OrderItem {
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "order_id", nullable = false)
+  @JsonIgnore // orders 필드 직렬화 방지
   private Orders orderId;
 
   @Column(name = "item_id")
   private Long itemId;
 
+  // 직렬화 시 Long orderId로 반환
+  @JsonProperty("orderId")
+  public Long getOrderId() {
+    return orderId != null ? orderId.getId() : null;
+  }
+  public void addOrders(Orders orders) {
+    this.orderId = orders;
+  }
+
+  @Override
+  public String toString() {
+    return "OrderItem{" +
+        "id=" + id +
+        ", quantity=" + quantity +
+        ", price=" + price +
+        ", totalPrice=" + totalPrice +
+        ", orderId=" + orderId +
+        ", itemId=" + itemId +
+        '}';
+  }
 }
