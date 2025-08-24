a52503056107466c866b3a8b16ca32c6f3d2aeb5
khg1109 <161204128+khg1109@users.noreply.github.com>
2025-08-24 16:15:21 +0900

    refactor: Image 엔티티 정의 및 JPA Repository 수정

--- image/src/main/java/com/example/image/domain/image/repository/ImageRepository.java
+++ image/src/main/java/com/example/image/domain/image/repository/ImageRepository.java
@@ -6,18 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
 
-    // 단건 (삭제되지 않은 것만)
     Optional<ImageEntity> findByIdAndDeletedFalse(Long id);
 
-    // 권한 체크 겸 단건
-    Optional<ImageEntity> findByIdAndStoreIdAndDeletedFalse(Long id, Long storeId);
+    List<ImageEntity> findByItemIdAndDeletedFalse(Long itemId);
 
-    // 상품별
-    List<ImageEntity> findAllByItemIdAndDeletedFalse(Long itemId);
-
-    // 스토어별
-    List<ImageEntity> findAllByStoreIdAndDeletedFalse(Long storeId);
-
-    // 존재 여부(권한 확인에 유용)
-    boolean existsByIdAndStoreIdAndDeletedFalse(Long id, Long storeId);
+    List<ImageEntity> findByStoreIdAndDeletedFalse(Long storeId);
 }
