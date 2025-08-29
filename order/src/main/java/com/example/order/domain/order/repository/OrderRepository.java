f9fe6cb9e21c13de7a830a25a168c8b86d24f8c7
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-08-29 16:14:19 +0900

    feat: order 조회 기능 구현

--- order/src/main/java/com/example/order/domain/order/repository/OrderRepository.java
+++ order/src/main/java/com/example/order/domain/order/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.example.order.domain.order.repository;
 
 import com.example.order.domain.order.repository.enums.OrderStatus;
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -17,7 +20,9 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
 
   List<Orders> findByUserId(Long userId);
 
-  List<Orders> findByStoreId(Long storeId);
+  List<Orders> findByStoreIdIn(List<Long> storeIds);
 
-
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT o FROM Orders o where o.id = :orderId")
+  Optional<Orders> findByIdPessimisticLock(Long orderId);
 }
