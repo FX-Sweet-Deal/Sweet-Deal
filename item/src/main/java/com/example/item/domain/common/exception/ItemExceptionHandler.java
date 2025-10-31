e69f28d521f8bab4f983c24d8ed0e9048c11a97f
김미래 <79897894+mirae7199@users.noreply.github.com>
2025-10-31 14:58:23 +0900

    feat: Item exception handling

--- item/src/main/java/com/example/item/domain/common/exception/ItemExceptionHandler.java
+++ item/src/main/java/com/example/item/domain/common/exception/ItemExceptionHandler.java
@@ -2,6 +2,12 @@ package com.example.item.domain.common.exception;
 
 import com.example.global.api.Api;
 import com.example.global.errorcode.ItemErrorCode;
+import com.example.item.domain.common.exception.item.InsufficientItemQuantityException;
+import com.example.item.domain.common.exception.item.InvalidExpiredAtException;
+import com.example.item.domain.common.exception.item.InvalidItemNameException;
+import com.example.item.domain.common.exception.item.InvalidItemPriceException;
+import com.example.item.domain.common.exception.item.InvalidItemStatusChangeException;
+import com.example.item.domain.common.exception.item.InvalidQuantityException;
 import com.example.item.domain.common.exception.item.ItemAlreadyExistsException;
 import com.example.item.domain.common.exception.item.ItemCannotDeleteException;
 import com.example.item.domain.common.exception.item.ItemNotFoundException;
@@ -35,6 +41,50 @@ public class ItemExceptionHandler {
     return ResponseEntity.status(HttpStatus.CONFLICT)
         .body(Api.error(ItemErrorCode.ITEM_ALREADY_DELETED));
   }
+
+  @ExceptionHandler(value = InvalidQuantityException.class)
+  public ResponseEntity<Api<Object>> invalidQuantityException(InvalidQuantityException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(ItemErrorCode.INVALID_ITEM_QUANTITY));
+  }
+
+  @ExceptionHandler(value = InvalidItemNameException.class)
+  public ResponseEntity<Api<Object>> invalidItemNameException(InvalidItemNameException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(ItemErrorCode.INVALID_ITEM_NAME));
+  }
+
+  @ExceptionHandler(value = InvalidExpiredAtException.class)
+  public ResponseEntity<Api<Object>> invalidExpireAtException(InvalidExpiredAtException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(ItemErrorCode.INVALID_ITEM_EXPIRED_DATE));
+  }
+
+  @ExceptionHandler(value = InvalidItemPriceException.class)
+  public ResponseEntity<Api<Object>> invalidItemPriceException(InvalidItemPriceException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(ItemErrorCode.INVALID_ITEM_PRICE));
+  }
+
+  @ExceptionHandler(value = InvalidItemStatusChangeException.class)
+  public ResponseEntity<Api<Object>> invalidItemStatusChangeException(InvalidItemStatusChangeException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(ItemErrorCode.INVALID_ITEM_STATUS_CHANGE));
+  }
+
+  @ExceptionHandler(value = InsufficientItemQuantityException.class)
+  public ResponseEntity<Api<Object>> insufficientItemQuantityException(InsufficientItemQuantityException e) {
+    log.error("", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(Api.error(ItemErrorCode.INSUFFICIENT_ITEM_QUANTITY));
+  }
+
+
 
 
 
